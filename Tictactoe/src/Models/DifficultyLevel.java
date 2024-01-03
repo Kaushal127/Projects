2ece4346b4f2d2ff5f3b6d229088bf417fa1ea25
Kaushal127 <kaushal.kaygude@gmail.com>
2024-01-03 16:24:55 +0530

    Design a parking lot

--- Tictactoe/src/Models/DifficultyLevel.java
+++
@@ -1,5 +0,0 @@
-package Models;
-
-public enum DifficultyLevel {
-    EASY , MEDIUM , HARD
-}
--- Tictactoe/src/botplayingstrategies/BotPlayingStrategy.java
+++
@@ -1,8 +0,0 @@
-package botplayingstrategies;
-
-import Models.Board;
-import Models.Cell;
-
-public interface BotPlayingStrategy {
-    Cell makeMove(Board board) ;
-}
